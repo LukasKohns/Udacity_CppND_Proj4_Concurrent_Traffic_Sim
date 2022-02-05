5a99f91a3eb9409fa77c21dfae80a322db1bdaf1
Lukas <lukaskohns@hotmail.de>
2022-02-05 15:00:23 +0100

    Task 5a Done, compiles and runs

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,19 +4,29 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
+
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+
+    // This is also from the Lesson 4 Excercises
+    std::unique_lock<std::mutex> uLock(_messageMutex);
+    _condition.wait(uLock, [this] {return !_queue.empty();});
+
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+
+    return msg;
 }
-*/
+
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+    // FP.4a : Done
+    // The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
 
     // This is mostly taken from the Lesson 4 Excercise
@@ -48,14 +58,15 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class.
+    // FP.2b : Done
+    // Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class.
     threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this)); 
 }
 
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
-    // FP.2a : Mostly Done, see sending of message
+    // FP.2a : Done
     // Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
