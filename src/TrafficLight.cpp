de7808766295f7cf2e89e710fa443efb977d1e87
Lukas <lukaskohns@hotmail.de>
2022-02-05 15:26:16 +0100

    Task 6 Done, compiles and runs, red color of trafficlight not displayed

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -46,7 +46,8 @@ TrafficLight::TrafficLight()
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
+    // FP.5b : Done
+    // add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
     while(true){
